bc1cc580925c33c87a5afa8565ab71f473276188
Oeslei Kuhn <oesleik@gmail.com>
2025-03-30 22:59:48 -0300

    Using HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import "./index.css"
 import { ModeSelectionRoute } from "./routes/ModeSelectionRoute.tsx";
 import { TimerRoute } from "./routes/TimerRoute.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-	<BrowserRouter>
+	<HashRouter>
 		<Routes>
 			<Route path="/" element={<ModeSelectionRoute />} />
 			<Route path="/timer/:mode" element={<TimerRoute />} />
 		</Routes>
-	</BrowserRouter>
+	</HashRouter>
   </StrictMode>,
 )
