dc2f1832b31cad01bb142e27cf41e561b8421a80
Oeslei Kuhn <oesleik@gmail.com>
2025-09-20 00:06:16 -0300

    Dropdown component and icons

--- src/pages/ModeSelectionPage.tsx
+++ src/pages/ModeSelectionPage.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router";
 import { getAvailableModes } from "../hooks/useModeSettings";
 import { CustomRef, isCustomRef, removeCustomSettingsFromStorage } from "../hooks/useCustomModeSettings";
 import { useState } from "react";
+import { Trash2Icon, TrashIcon } from "lucide-react";
 
 export const ModeSelectionPage = () => {
 	const [_, forceUpdate] = useState(0);
@@ -34,10 +35,7 @@ function RemoveButton({ ref, refresh }: { ref: CustomRef, refresh: () => void })
 		}
 	};
 
-	return <button type="button" onClick={handleClick} className="px-4 py-2 bg-echo-muted-500 text-echo-black-500 font-medium rounded-lg border-2 focus:border-echo-white-500 hover:border-echo-white-500 cursor-pointer">
-		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-			<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path>
-			<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path>
-		</svg>
+	return <button type="button" onClick={handleClick} className="px-3 py-2 bg-echo-muted-500 text-echo-black-500 font-medium rounded-lg border-2 focus:border-echo-white-500 hover:border-echo-white-500 cursor-pointer">
+		<Trash2Icon />
 	</button>
 }
--- src/pages/TimerControlPage.tsx
+++ src/pages/TimerControlPage.tsx
@@ -8,6 +8,7 @@ import { SoundVolumeState } from "../hooks/useSoundVolumeState";
 import { Slider } from "../components/Slider";
 import { useClickOutside } from "../hooks/useClickOutside";
 import { findNextTimerViewMode } from "../hooks/useTimerViewModeState";
+import { Volume2, VolumeOff } from "lucide-react";
 
 export const TimerControlPage = ({ modeSettings, roundSettings }: {
 	modeSettings: ModeSettings,
@@ -103,15 +104,11 @@ const TimerViewActions = ({ viewMode, setViewMode, hasExercises }: MainTimerStat
 const SoundToggleButton = forwardRef<HTMLButtonElement, { isMuted: boolean, active: boolean, onClick: () => void }>(({ isMuted, active, onClick }, ref) => {
 	if (isMuted) {
 		return <Button ref={ref} active={active} onClick={onClick}>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-				<path strokeLinecap="round" strokeLinejoin="round" d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-			</svg>
+			<VolumeOff />
 		</Button>;
 	}
 
 	return <Button ref={ref} active={active} onClick={onClick}>
-		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-			<path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-		</svg>
+		<Volume2 />
 	</Button>;
 });
